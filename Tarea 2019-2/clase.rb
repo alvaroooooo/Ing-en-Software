ff7a4ea447ab8d27a3d8a0134bbd4fc8bf0d14fb
Alvaro <ajescalona@uc.cl>
2021-02-01 19:24:03 -0300

    Ultimos cambios :green_heart:

--- Tarea 2019-2/clase.rb
+++ Tarea 2019-2/clase.rb
@@ -1,20 +1,49 @@
-class clase
-
+class Curso
+    attr_accessor :informacion, :sigla
     def initialize(sigla, informacion)
         @sigla = sigla 
         @informacion = informacion
+        # Info es una hash table que contiene id : nota_alumno
     end
 
-    def mejor_nota()
+    def mejor_nota
+        mayor = informacion.values.max
+        nom_mayores = Array.new
+        informacion.each do |key, value|
+            if value == mayor
+                nom_mayores.append(key)
+            end
+        end
+        puts "La mejor nota es #{mayor} de #{nom_mayores.join(", ")}"
     end
 
-    def nota_promedio()
+    def nota_promedio
+        notas = informacion.values
+        average = (notas.sum / notas.size).round(2)
+        puts "El promedio es #{average}"
     end
     
-    def top_n_notas()
+    def top_n_notas(cantidad_req)
+        notas_orden = informacion.values.sort.reverse
+        p notas_orden
+        puts notas_orden.slice(0,cantidad_req)
     end
 
-    def alumnos_reprobados()
+    def alumnos_reprobados
+        list_reprobados = Array.new
+        informacion.each do |id, nota|
+            if nota <= 4
+                list_reprobados.append("#{id} con un #{nota}")
+            end
+        end
+        puts list_reprobados
     end
 end
 
+# Clase de prueba 
+curso = Curso.new("ICS2313", {"1231" => 5.2, "1432" => 6, "5432" => 5.5, "9231" => 3.2, "4141" => 3.8})
+
+curso.mejor_nota
+curso.nota_promedio
+curso.top_n_notas(2)
+curso.alumnos_reprobados
--- Tarea 2019-2/personas.rb
+++ Tarea 2019-2/personas.rb
@@ -33,14 +33,31 @@ class Alumno < Persona
 end
 
 class Profesor < Persona
+    attr_accessor :lista_alumnos
 
-    def initialize(id, nombre, edad, sigla_curso)
+    def initialize(id, nombre, edad, sigla_curso, lista_alumnos)
         super(id, nombre, edad)
         @sigla_curso = sigla_curso
+        @lista_alumnos = procesar_lista(lista_alumnos)
     end
     
     def cantidad_de_alumnos()
         puts "\nHola a todos, me llamaron"
     end
+
+    def procesar_lista(lista)
+        por_alumno = lista.split("-")
+        ordenado = Array.new
+        por_alumno.each do |item|
+            item = item.split(":")
+            item[1] = item[1].to_f
+            ordenado.append(item)
+        end
+        return ordenado
+    end
 end
+
+#coso = Profesor.procesar_lista("4101:6.8-4123:4.2")
+new_profe = Profesor.new("3213", "Juanito", "32", "ICS2132","4101:6.8-4123:4.2")
+p new_profe.lista_alumnos
 
